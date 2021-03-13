2bf30098ec615f6567cb8e9da49d34de26b3c559
Steven Kim <97stevenkim@gmail.com>
2021-03-12 21:10:32 -0500

    rainbow async await then

--- app.js
+++ app.js
@@ -1,48 +1,35 @@
-
-const playera = {
-    score: 0,
-    button: document.querySelector("#a"),
-    display: document.querySelector("#playera")
-}
-const playerb = {
-    score: 0,
-    button: document.querySelector("#b"),
-    display: document.querySelector("#playerb")
-}
-const playerList = [playera, playerb];
-
-
-let maxscore = +document.querySelector("#maxscore > [selected]").innerHTML;
-let maxselect = document.querySelector('#maxscore');
-
-for (let player of playerList) {
-    player.button.addEventListener('click', function() {
-        changeScore(player);
-    });
+function delayedColorChange(color, time) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            rej();
+        }, time);
+    })
 }
 
-function changeScore(player) {
-    player.score += 1;
-    player.display.innerHTML = player.score;
-    if (player.score == maxscore) {
-        for (let player of playerList) {
-            player.button.disabled = true;
-            player.display.classList.add('has-text-danger');
-        }
-        player.display.classList.remove('has-text-danger');
-        player.display.classList.add('has-text-success');
+
+// delayedColorChange('red', 1000)
+//     .then(function() { return delayedColorChange('blue', 1000) })
+//     .then(function() { return delayedColorChange('orange', 1000) })
+//     .then(function() { return delayedColorChange('yellow', 1000) })
+//     .then(function() { delayedColorChange('blue', 1000) });
+
+async function rainbow() {
+    await delayedColorChange('purple', 700);
+    await delayedColorChange('orange', 700);
+    await delayedColorChange('blue', 700);
+    await delayedColorChange('violet', 700);
+    return "all done?";
+}
+
+async function printRainbow() {
+    let result = await rainbow();
+    console.log(result)
+    try {
+
+    } catch (e) {
+        
     }
 }
 
-document.querySelector('#reset').addEventListener('click', function() {
-    for (player of playerList) {
-        player.button.disabled = false;
-        player.score = 0;
-        player.display.classList.remove('has-text-danger', 'has-text-success');
-        player.display.innerText = "0";
-    }
-});
-
-document.querySelector('#maxscore').addEventListener('input', function() {
-    maxscore = maxselect.value;
-});
+printRainbow();
