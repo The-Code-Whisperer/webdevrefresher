705e98989d621cb8b665a2198c9d4d921e014e21
Steven Kim <97stevenkim@gmail.com>
2021-03-18 01:38:59 -0400

    classes

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
-
-
-
-const form = document.querySelector('form');
-
-form.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    const shows = await getTVShows(form.elements.query.value);
-    document.querySelectorAll('img').forEach(node => node.remove());
-    for (let show of shows) {
-        if (show.show.image) {
-            const img = document.createElement('img');
-            img.src = show.show.image.medium;
-            document.body.append(img);
-        }
+// factory function return an object with preset properties given parameter arguments, as well as methods.
+// new allows us to use constructor functions, which after being instantiated lets us add prototype methods.
+// new lets us also use class which is even better, because you can add prototype methods without leaving the declaration (but outside the constructor, unless you want the function to be defined inside the instantiation.)
+class Color {
+    constructor(r, g, b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
     }
-    form.elements.query.value = '';
-
-});
-
-async function getTVShows(searchTerm) {
-    const config = {'params': {'q': searchTerm}}
-    const data = await axios.get('http://api.tvmaze.com/search/shows', config);
-    return data.data;
+    rgb() {
+        const {r, g, b} = this;
+        return `${r}, ${g}, ${b}`;
+    }
 }
+
+let red = new Color(255, 0);
+console.log(red.rgb());
