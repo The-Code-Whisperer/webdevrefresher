a05d888adfd9ee6b493bf548ed537fee7210c6f8
Steven Kim <97stevenkim@gmail.com>
2021-03-22 17:46:01 -0400

    idlescape api calls in langdetect

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-// factory function return an object with preset properties given parameter arguments, as well as methods.
-// new allows us to use constructor functions, which after being instantiated lets us add prototype methods.
-// new lets us also use class which is even better, because you can add prototype methods without leaving the declaration (but outside the constructor, unless you want the function to be defined inside the instantiation.)
-class Color {
-    constructor(r, g, b, name) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.name = name;
-    }
-    rgb() {
-        const {r, g, b} = this;
-        return `${r}, ${g}, ${b}`;
-    }
-}
+var figlet = require('figlet');
+var colors = require('colors');
 
-let red = new Color(255, 0);
-console.log(red.rgb());
+ 
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data.green)
+});
